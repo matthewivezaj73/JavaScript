89c03a57843d5cf93c1b84e91085e5d2ae9ac5b1
Matthew Ivezaj <matthew.ivezaj73@gmail.com>
2022-10-01 22:22:06 -0400

    Cut code out.

--- first_project/app.js
+++ first_project/app.js
@@ -32,13 +32,3 @@ console.timeEnd('Hello');
     comments.
 */
 
-//Var, let const.
-var my_name_is = "John Doe";
-console.log(my_name_is);
-my_name_is = "Steve Smith";
-console.log(my_name_is);
-
-var greeting;
-console.log(greeting);
-greeting = "Hello";
-console.log(greeting);
